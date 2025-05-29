45705783ca4a043724db5282836e2627a0ffb4e0
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-29 15:49:37 +0200

    [UPD] imposto express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world!!!")
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send('Movies API server')
+});
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
