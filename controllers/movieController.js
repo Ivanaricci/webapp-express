0a122582fffe3a77d3577032fe155e18cdfe08c6
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-29 20:19:01 +0200

    [UPD] Add rotta show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -9,7 +9,27 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-    console.log('Dettaglio flm')
+    // recupero l'id
+    const { id } = req.params
+
+    // query per il recupero dei film avente l'id recuperato
+    const movieSql = "SELECT * FROM movies WHERE id= ?";
+
+    // query per il recupero delle recensioni del film recuperato
+    const reviewsSql = `
+    SELECT *
+    FROM reviews
+    WHERE movie_id = ?
+    `
+
+    // eseguo la query per recuperare il film
+    connection.query(movieSql, [id], (err, movieResult) => {
+        if (err) return res.status(500).json({ error: "Database query failed" + err });
+
+        res.json(movieResult);
+    })
+
+
 }
 
 module.exports = {
