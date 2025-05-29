9c5586c6aa9bba45ba5f7bd2452bbe632074e266
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-29 16:10:36 +0200

    [UPD] Add variabili d' ambiente

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+// importo express
 const express = require('express');
+
+// inizializzo la variabile app con il metodo express
 const app = express();
-const port = 3000;
+
+// definisco il numero di porta su cui il server deve rimanere in ascolto
+const port = process.env.SERVER_PORT || 3000;
+
+// uso il middleware per gli asset statici
 app.use(express.static('public'));
+
+// uso il middlware per il parsing del body delle richieste
 app.use(express.json());
+
+// definisco l'entry point
 app.get("/", (req, res) => {
     res.send('Movies API server')
 });
+
+// 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
--- data/db.js
+++ data/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 // creao la connessione al db
 const connection = mysql.createConnection({
-    host: "",
-    port: "",
-    user: "",
-    password: "",
-    database: ""
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 // effettuo la connessione
