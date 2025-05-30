45e226b8772fe59b28183b6643f4127242526f4c
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-30 08:57:10 +0200

    [UPD] Add recensioni

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -26,7 +26,24 @@ const show = (req, res) => {
     connection.query(movieSql, [id], (err, movieResult) => {
         if (err) return res.status(500).json({ error: "Database query failed" + err });
 
-        res.json(movieResult);
+        // se non trovo il film mostro errore 404
+        if (movieResult.length === 0 || movieResult[0].id === null) {
+            return res.status(404).json({ error: "Not found" })
+        }
+
+        const movie = movieResult[0];
+
+        // eseguo la query per il recupero delle recensioni
+        connection.query(reviewsSql, [id], (err, reviewsResult) => {
+            if (err) return res.status(500).json({ error: "Database query failed" + err });
+
+            // aggiungo le recensioni
+            movie.reviews = reviewsResult;
+
+            res.json(movieResult);
+        })
+
+
     })
 
 
